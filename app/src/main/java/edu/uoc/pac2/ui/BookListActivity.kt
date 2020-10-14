aecdc2276fcbc8b42a16f070405578a826cd1eea
Chenghuan Ji <chenghuanmiguel@hotmail.com>
2020-10-14 20:15:29 +0200

    Ejercicio 7 Terminado

--- app/src/main/java/edu/uoc/pac2/ui/BookListActivity.kt
+++ app/src/main/java/edu/uoc/pac2/ui/BookListActivity.kt
@@ -11,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import edu.uoc.pac2.MyApplication
@@ -18,6 +20,8 @@ import edu.uoc.pac2.R
 import edu.uoc.pac2.data.Book
 import edu.uoc.pac2.data.BooksInteractor
 import edu.uoc.pac2.data.FirestoreBookData
+import kotlinx.android.synthetic.main.activity_book_list.*
+import kotlinx.android.synthetic.main.view_book_list.*
 
 /**
  * An activity representing a list of Books.
@@ -25,6 +29,7 @@ import edu.uoc.pac2.data.FirestoreBookData
 class BookListActivity : AppCompatActivity() {
 
     private val TAG = "BookListActivity"
+
     private val db = Firebase.firestore
 
     private lateinit var adapter: BooksListAdapter
@@ -44,7 +49,17 @@ class BookListActivity : AppCompatActivity() {
 
         //FirestoreBookData.addBooksDataToFirestoreDatabase() // Ejecutado solo una vez. Para añadir los datos a firestore
 
+        //Admob
+        loadAdMob()
 
+    }
+
+    //Admob
+    //Recordar reemplazar el adUnitId del layout cuando lanzes la aplicación
+    private fun loadAdMob(){
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
     // Init Top Toolbar
@@ -56,7 +71,7 @@ class BookListActivity : AppCompatActivity() {
 
     // Init RecyclerView
     private fun initRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.book_list)
+        val recyclerView = book_list//findViewById<RecyclerView>(R.id.book_list)
         // Set Layout Manager
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
